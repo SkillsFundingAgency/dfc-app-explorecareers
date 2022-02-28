6b53c62769e6ea86662a8244cb1e4646935d64aa
ravi-methods <83820221+ravi-methods@users.noreply.github.com>
2022-02-28 14:34:16 +0000

    FT2-1295: Integration tests added. (#34)

---
+++ DFC.App.ExploreCareers.IntegrationTests/ControllerTests/ExploreCareersRouteTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+using DFC.App.ExploreCareers.Data.Models.ContentModels;
+
+using FakeItEasy;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace DFC.App.ExploreCareers.IntegrationTests.ControllerTests
+{
+    [Trait("Category", "ExploreCareers Controller Integration Tests")]
+    public class ExploreCareersRouteTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> factory;
+
+        public ExploreCareersRouteTests(CustomWebApplicationFactory<Startup> factory) =>
+            this.factory = factory;
+
+        [Fact]
+        public async Task GetBodyEndpointReturnSuccessAndCorrectContentType()
+        {
+            // Arrange
+            string category = "something";
+            var uri = new Uri($"/explore-careers/body", UriKind.Relative);
+            var client = GetClient();
+
+            A.CallTo(() => factory.FakeDocumentService.GetAllAsync(A<string>._)).Returns(new List<JobCategoryContentItemModel>
+            {
+                new JobCategoryContentItemModel { CanonicalName = category, Title = category }
+            });
+
+            // Act
+            var response = await client.GetAsync(uri);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Headers.ContentType.MediaType.Should().Be(MediaTypeNames.Text.Html);
+        }
+
+        [Fact]
+        public async Task GetHeadEndpointReturnSuccessAndCorrectContentType()
+        {
+            // Arrange
+            var uri = new Uri($"/explore-careers/head", UriKind.Relative);
+            var client = GetClient();
+
+            // Act
+            var response = await client.GetAsync(uri);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Headers.ContentType.MediaType.Should().Be(MediaTypeNames.Text.Html);
+        }
+
+        [Fact]
+        public async Task GetBodyTopEndpointReturnSuccessAndCorrectContentType()
+        {
+            // Arrange
+            var uri = new Uri($"/explore-careers/bodytop", UriKind.Relative);
+            var client = GetClient();
+
+            // Act
+            var response = await client.GetAsync(uri);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Headers.ContentType.MediaType.Should().Be(MediaTypeNames.Text.Html);
+        }
+
+        private HttpClient GetClient()
+        {
+            var client = factory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(MediaTypeNames.Text.Html));
+            return client;
+        }
+    }
+}
---
+++ DFC.App.ExploreCareers.IntegrationTests/ControllerTests/SearchAutocompleteRouteTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using DFC.App.ExploreCareers.Models;
+
+using FakeItEasy;
+
+using FluentAssertions;
+
+using Microsoft.AspNetCore.Http;
+
+using Xunit;
+
+namespace DFC.App.ExploreCareers.IntegrationTests.ControllerTests
+{
+    [Trait("Category", "SearchAutocomplete Controller Integration Tests")]
+    public class SearchAutocompleteRouteTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> factory;
+
+        public SearchAutocompleteRouteTests(CustomWebApplicationFactory<Startup> factory) =>
+            this.factory = factory;
+
+        [Fact]
+        public async Task GetHeadEndpointReturnsNoContent()
+        {
+            // Arrange
+            var uri = new Uri("/searchautocomplete/head", UriKind.Relative);
+            var client = factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(uri);
+
+            // Assert
+            response.StatusCode.Should().Be(StatusCodes.Status204NoContent);
+        }
+
+        [Fact]
+        public async Task GetEndpointReturnsSuccessAndCorrectContent()
+        {
+            // Arrange
+            var uri = new Uri("/searchautocomplete?term=something", UriKind.Relative);
+            var client = factory.CreateClient();
+            var expectedResults = new List<AutoCompleteModel>
+            {
+                new AutoCompleteModel{ Label = "A", Category = "A"},
+                new AutoCompleteModel{ Label = "B", Category = "B"},
+            };
+            A.CallTo(() => factory.FakeAzureSearchService.GetSuggestionsAsync(A<string>._, A<int>._, A<bool>._)).Returns(expectedResults);
+
+            // Act
+            var response = await client.GetAsync(uri);
+
+            // Assert
+            response.StatusCode.Should().Be(StatusCodes.Status200OK);
+            var result = await response.Content.ReadAsAsync<List<AutoCompleteModel>>();
+            result.Should().BeEquivalentTo(expectedResults);
+        }
+    }
+}
