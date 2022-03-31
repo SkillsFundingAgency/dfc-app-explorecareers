7f1106925436028de4cdf2090ec5c1a507fc9297
vaibe <30327964+vaibe@users.noreply.github.com>
2022-03-31 10:21:38 +0100

    UI tests (#35)
    * UI Tests project added with scaffolding.
    
    * EC base automation
    
    * Things ok? run.
    
    * Appsettings file added back.
    
    * .
    
    * Style cop edits
    
    * Job profiles links and breadcrumb verification
    
    * Test: Compare Job profile search counts in different environments
    
    * Minor edits on: Compare Job profile search counts in different environments
    
    * Auto for: Compare Job profile search result counts in different environments
    
    * Removed csv
    
    * Separation into different features
    
    * TCB01 removed duplicate Job profiles
    
    * Screenshot on failure included
    
    * Ignored migration specific tests.
    
    Co-authored-by: ravi-methods <ravi.mittal@methods.co.uk>

---
+++ DFC.App.ExploreCareers.UI.FunctionalTests/Hooks/AfterScenario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using DFC.App.ExploreCareers.UI.FunctionalTests.Model;
+using DFC.TestAutomation.UI.Extension;
+using OpenQA.Selenium.Remote;
+using TechTalk.SpecFlow;
+
+namespace DFC.App.ExploreCareers.UI.FunctionalTests.Hooks
+{
+    [Binding]
+    public class AfterScenario
+    {
+        public AfterScenario(ScenarioContext context)
+        {
+            Context = context ?? throw new NullReferenceException($"The scenario context is null. The {nameof(AfterScenario)} class cannot be initialised.");
+        }
+
+        private ScenarioContext Context { get; set; }
+
+        [AfterScenario(Order = 0)]
+        public async Task UpdateBrowserStack()
+        {
+            var browserHelper = this.Context.GetHelperLibrary<AppSettings>().BrowserHelper;
+            if (browserHelper.IsExecutingInBrowserStack())
+            {
+                var sessionId = (this.Context.GetWebDriver() as RemoteWebDriver).SessionId.ToString();
+                var browserStackHelper = this.Context.GetHelperLibrary<AppSettings>().BrowserStackHelper;
+
+                if (this.Context.TestError != null)
+                {
+                    var errorMessage = this.Context.TestError.InnerException.Message;
+                    await browserStackHelper.SetTestToFailedWithReason(sessionId, errorMessage);
+                }
+                else
+                {
+                    await browserStackHelper.SetTestToPassed(sessionId);
+                }
+            }
+        }
+
+        [AfterScenario(Order = 1)]
+        public void DisposeWebDriver()
+        {
+            var webDriver = this.Context.GetWebDriver();
+            webDriver?.Quit();
+            webDriver?.Dispose();
+        }
+    }
+}
---
+++ DFC.App.ExploreCareers.UI.FunctionalTests/Pages/ExploreCareersPage.cs
@@ -0,0 +1,293 @@
+﻿using DFC.App.ExploreCareers.UI.FunctionalTests.Model;
+using DFC.App.ExploreCareers.UI.FunctionalTests.StepDefinitions;
+using DFC.App.ExploreCareers.UI.FunctionalTests.Support;
+using DFC.App.ExploreCareers.UI.FunctionalTests.Support.Poco;
+using DFC.TestAutomation.UI.Extension;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace DFC.App.ExploreCareers.UI.FunctionalTests.Pages
+{
+    internal class ExploreCareersPage
+    {
+        private readonly ScenarioContext scenarioContext;
+
+        public ExploreCareersPage(ScenarioContext context)
+        {
+            scenarioContext = context;
+        }
+
+        public static string Endpoint { get; set; }
+
+        public static string Environment { get; set; }
+
+        public string NavigationPoint { get; set; }
+
+        private IWebElement SearchField => scenarioContext.GetWebDriver().FindElement(By.Id("SearchTerm"));
+
+        private IWebElement SearchButton => scenarioContext.GetWebDriver().FindElement(By.ClassName("submit"));
+
+        private IWebElement PageHeading => scenarioContext.GetWebDriver().FindElement(By.Id("site-header"));
+
+        public void ClickLinkJobCategory(string jobCategory)
+        {
+            scenarioContext.GetWebDriver().FindElement(By.XPath("//*[@class='govuk-link'][contains(text(),'" + jobCategory + "')]")).Click();
+        }
+
+        public void NavigateToPage(string resourceOne, string resourceTwo = null)
+        {
+            Endpoint = this.scenarioContext.GetSettingsLibrary<AppSettings>().AppSettings.AppBaseUrl.ToString().Replace("job-profiles/", string.Empty);
+
+            switch (resourceOne)
+            {
+                case "Explore careers":
+                    this.scenarioContext.GetWebDriver().Url = Endpoint + "explore-careers";
+                    break;
+                case "Job profiles":
+                    this.scenarioContext.GetWebDriver().Url = Endpoint + "job-profiles/admin-assistant";
+                    Devices.ScrollIntoView(this.scenarioContext.GetWebDriver(), scenarioContext.GetWebDriver().FindElement(By.Id("search-main")));
+                    break;
+                case "Search results":
+                    this.scenarioContext.GetWebDriver().Url = Endpoint + "search-results";
+                    break;
+                case "Job categories":
+                    this.scenarioContext.GetWebDriver().Url = Endpoint + "job-categories/" + ProcessResourceTwo(resourceTwo);
+                    break;
+                case "Production":
+                    this.scenarioContext.GetWebDriver().Url = "https://nationalcareers.service.gov.uk/" + ProcessResourceTwo(resourceTwo);
+                    break;
+            }
+
+            GetEnvironment();
+        }
+
+        public void NavigateToProd(string resource, string parameter)
+        {
+            string endPoint = "https://nationalcareers.service.gov.uk/";
+            this.scenarioContext.GetWebDriver().Url = endPoint + resource + ProcessResourceTwo(parameter);
+            NavigationPoint = endPoint;
+        }
+
+        public string ProcessResourceTwo(string resourceTwo)
+        {
+            string secondResource = string.Empty;
+
+            switch (resourceTwo)
+            {
+                case "Administration":
+                    secondResource = "Administration";
+                    break;
+                case "Animal care":
+                    secondResource = "animal-care";
+                    break;
+                case "Beauty and wellbeing":
+                    secondResource = "beauty-and-wellbeing";
+                    break;
+                case "Business and finance":
+                    secondResource = "business-and-finance";
+                    break;
+                case "Computing, technology and digital":
+                    secondResource = "computing-technology-and-digital";
+                    break;
+                case "Construction and trades":
+                    secondResource = "construction-and-trades";
+                    break;
+                case "Creative and media":
+                    secondResource = "creative-and-media";
+                    break;
+                case "Delivery and storage":
+                    secondResource = "delivery-and-storage";
+                    break;
+                case "Emergency and uniform services":
+                    secondResource = "emergency-and-uniform-services";
+                    break;
+                case "Engineering and maintenance":
+                    secondResource = "engineering-and-maintenance";
+                    break;
+                case "Environment and land":
+                    secondResource = "environment-and-land";
+                    break;
+                case "Government services":
+                    secondResource = "government-services";
+                    break;
+                case "Healthcare":
+                    secondResource = "healthcare";
+                    break;
+                case "Home services":
+                    secondResource = "home-services";
+                    break;
+                case "Hospitality and food":
+                    secondResource = "hospitality-and-food";
+                    break;
+                case "Law and legal":
+                    secondResource = "law-and-legal";
+                    break;
+                case "Managerial":
+                    secondResource = "Managerial";
+                    break;
+                case "Manufacturing":
+                    secondResource = "Manufacturing";
+                    break;
+                case "Retail and sales":
+                    secondResource = "retail-and-sales";
+                    break;
+                case "Science and research":
+                    secondResource = "science-and-research";
+                    break;
+                case "Social care":
+                    secondResource = "social-care";
+                    break;
+                case "Sports and leisure":
+                    secondResource = "sports-and-leisure";
+                    break;
+                case "Teaching and education":
+                    secondResource = "teaching-and-education";
+                    break;
+                case "Transport":
+                    secondResource = "Transport";
+                    break;
+                case "Travel and tourism":
+                    secondResource = "travel-and-tourism";
+                    break;
+                case "search results":
+                    secondResource = "search-results";
+                    break;
+                default:
+                    secondResource = string.Empty;
+                    break;
+            }
+
+            return secondResource;
+        }
+
+        public void EnterSearchTerm(string searchTerm)
+        {
+            ClearSearchField();
+            SearchField.SendKeys(searchTerm);
+        }
+
+        public void SelectFromAutosuggest(string autosuggested)
+        {
+           SearchField.Click();
+           scenarioContext.GetWebDriver().FindElement(By.XPath("//div[@class='header-search-content']//following-sibling::ul/li/div[contains(text(), '" + autosuggested + "')]")).Click();
+        }
+
+        public string GetSelectedSearchTerm()
+        {
+            return SearchField.GetAttribute("value");
+        }
+
+        public void ClickSearchButton()
+        {
+            SearchButton.Click();
+        }
+
+        public void ClearSearchField()
+        {
+            SearchField.Clear();
+        }
+
+        public string GetUrl()
+        {
+            return scenarioContext.GetWebDriver().Url;
+        }
+
+        public void ClickEnterInSearchField()
+        {
+            SearchField.Click();
+            SearchField.SendKeys(Keys.Enter);
+        }
+
+        public IList<IWebElement> GetJobCategoryList()
+        {
+            IList<IWebElement> allList = scenarioContext.GetWebDriver().FindElements(By.CssSelector(".govuk-list.homepage-jobcategories > li > a"));
+
+            return allList;
+        }
+
+        public bool VerifyJobCategoryList(IEnumerable<JobCategories> expectedJobCategories)
+        {
+            bool a_and_b_checks = false;
+
+            //A - Check.
+            //Convert IEnumerable expected results to string
+            string[] expected = expectedJobCategories.Select(p => p.JobCategory).ToArray();
+
+            //Get actual list from the UI
+            IList<IWebElement> actualJobCategoriesList = scenarioContext.GetWebDriver().FindElements(By.CssSelector(".govuk-list.homepage-jobcategories > li > a"));
+
+            //translate IWebElements above into a collection of strings so they can be compared
+            IEnumerable<string> actual = actualJobCategoriesList.Select(i => i.Text);
+
+            //determines, as bool, if items in 1 and 2 are present in the other
+            var optionsVerified = expected.All(d => actual.Contains(d));
+
+            //B - Check.
+            int noOfActualElements = actualJobCategoriesList.Count;
+            bool optionsEqual = false;
+
+            if (expected.Length == noOfActualElements)
+            {
+                optionsEqual = true;
+            }
+
+            if (optionsVerified == true && optionsEqual == true)
+            {
+                a_and_b_checks = true;
+            }
+
+            return a_and_b_checks;
+        }
+
+        public IList<IWebElement> GetList(string listToExamine)
+        {
+            IList<IWebElement> theList = null;
+
+            switch (listToExamine)
+            {
+                case "Job categories":
+                    theList = scenarioContext.GetWebDriver().FindElements(By.CssSelector(".govuk-list.homepage-jobcategories li a"));
+                    break;
+                case "Job profiles":
+                    theList = scenarioContext.GetWebDriver().FindElements(By.CssSelector(".govuk-list.job-categories_items li h2 a"));
+                    break;
+                case "Other job categories":
+                    theList = scenarioContext.GetWebDriver().FindElements(By.CssSelector(".govuk-list.font-xsmall > li > a"));
+                    break;
+            }
+
+            return theList;
+        }
+
+        public bool VerifyOrdering()
+        {
+            var x = GetJobCategoryList().Select(item => item.Text.Replace(System.Environment.NewLine, string.Empty));
+            var sorted = new List<string>();
+            sorted.AddRange(x.OrderBy(o => o));
+
+            return x.SequenceEqual(sorted);
+        }
+
+        public string GetPageHeading()
+        {
+            return PageHeading.Text;
+        }
+
+        public void GetEnvironment()
+        {
+            if (Endpoint.Contains("staging"))
+            {
+                Environment = "Pre-Production";
+            }
+            else
+            {
+                Environment = Devices.Between(Endpoint, "https://", "-beta.").ToUpper(CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
