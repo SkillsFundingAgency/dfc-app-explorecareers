5ac84713c20a9d40158687d31d9c551b8014eb19
hnandra <32364605+hnandra@users.noreply.github.com>
2024-01-12 17:00:08 +0000

    Added GraphQlService files

---
+++ DFC.App.ExploreCareers/GraphQl/GraphQlService.cs
@@ -0,0 +1,41 @@
+﻿using DFC.App.ExploreCareers.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DFC.Common.SharedContent.Pkg.Netcore;
+using DFC.Common.SharedContent.Pkg.Netcore.Interfaces;
+using AutoMapper;
+using NHibernate.Engine;
+using DFC.Common.SharedContent.Pkg.Netcore.Repo;
+
+namespace DFC.App.ExploreCareers.GraphQl
+{
+    public class GraphQlService : IGraphQlService
+    {
+        private readonly IRedisCMSRepo redisCMSRepo;
+        private string status = string.Empty;
+
+        public GraphQlService(IRedisCMSRepo redisCMSRepo)
+        {
+            this.redisCMSRepo = redisCMSRepo;
+        }
+
+        public async Task<List<JobCategoryViewModel>> GetJobCategoriesAsync()
+        {
+            status = "PUBLISHED";
+            string query = @$"
+                query MyQuery {{
+                    jobProfileCategory(status: {status}){{
+                        displayText
+                        pageLocation{{
+                            fullUrl
+                            }}                        
+                        }}
+                    }}
+            ";
+
+            var response = await redisCMSRepo.GetGraphQLData<List<JobCategoryViewModel>>(query, "explorecareers/jobcategories");
+
+            return response;
+        }
+    }
+}
---
+++ DFC.App.ExploreCareers/GraphQl/IGraphQlService.cs
@@ -0,0 +1,11 @@
+﻿using DFC.App.ExploreCareers.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DFC.App.ExploreCareers.GraphQl
+{
+    public interface IGraphQlService
+    {
+        Task<List<JobCategoryViewModel>> GetJobCategoriesAsync();
+    }
+}
