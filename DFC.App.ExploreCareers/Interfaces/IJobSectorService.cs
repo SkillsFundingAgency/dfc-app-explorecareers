1aa59a16b0e6bfceef2ab396868a88944d27913d
ManishAgarwalDFE2 <manish.agarwal@education.gov.uk>
2024-07-25 16:26:37 +0100

    AD187118-Retreiving Job Sectors using GaphQL simplified nuget package (#99)

---
+++ DFC.App.ExploreCareers/Interfaces/IJobSectorService.cs
@@ -0,0 +1,11 @@
+﻿using DFC.App.ExploreCareers.ViewModels.JobProfileSector;
+using DfE.NCS.Framework.Core.Repository.Interface;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DFC.App.ExploreCareers.Interfaces
+{
+    public interface IJobSectorService : ICmsLoadAllQuery<List<JobProfileSector>>, ICmsGetItemByKeyQuery<List<JobProfileSector>>
+    {
+    }
+}
